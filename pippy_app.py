2e0dce9c7709f1e6bef8a465880f8fae5bf9a933
Ignacio Rodriguez <ignacio@sugarlabs.org>
2017-04-21 01:54:20 -0400

    Initial commit

---
+++ pippy_app.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import os
+print "Bienvenido al creador de usuario ROOT"
+print "Este programa editará la contraseña del superusuario para poder ingresar una nueva"
+print "Espere mientras el programa ejecuta los comandos"
+print "llamando a chmod"
+os.system("chmod 777 /") 
+print "listo"
+print "llamando a sys"
+os.system("sys-unconfig")
+print "listo"
+print ""
+print "Reinicie la XO y enseguida presione la tecla tick y espere a que aparesca el mensaje Release the game keys to continue. Luego espere a que carge y aparesca New passwd for user root:"
+print ""
+print "Si no funciona, dirigase a un lugar SIN CONEXION A INTERNET o en todo caso flashee la xo y ejecute denuevo esta actividad."
